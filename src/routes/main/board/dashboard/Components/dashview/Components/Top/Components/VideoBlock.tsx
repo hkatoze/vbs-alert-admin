420a33f1a1de2ce9d479d7f01266492dd29be8de
hkatoze <harounakinda@outlook.fr>
2023-12-27 15:38:50 +0000

    videoblock update

--- src/routes/main/board/dashboard/Components/dashview/Components/Top/Components/VideoBlock.tsx
+++ src/routes/main/board/dashboard/Components/dashview/Components/Top/Components/VideoBlock.tsx
@@ -1,13 +1,19 @@
 import "./VideoBlock.css";
 import VIDEO from "../../../../../../../../../assets/video.mp4";
+import { Link } from "react-router-dom";
 const VideoBlock = () => {
   return (
     <div className="videoBlock flex">
-      <h1>Create and sell extraordinary products</h1>
-      <p>The world's fast growing industry today are natural made products !</p>
+      <h1>Empower your phone to be your personal superhero.</h1>
+      <p>Choose VBSAlert because every second matters.</p>
       <div className="buttons flex">
-        <button className="btn">Explore More</button>
-        <button className="btn transparent">Top sellers</button>
+        <Link to="/mainpage/companiesboard/addCompany">
+          <button className="btn">New company</button>
+        </Link>
+
+        <Link to="/mainpage/dashboard/alertsHistory" className="flex link">
+          <button className="btn transparent">Alerts history</button>
+        </Link>
       </div>
       <div className="videoDiv">
         <video src={VIDEO} autoPlay loop muted></video>
